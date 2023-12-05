7c6f3d6278e86286900d9f6d36f7016d7f5d753d
Jitendra Khatnawaliy <jeetu,nawal6290@gmail.com>
2023-12-05 20:06:42 +0530

    Hyperparameter train

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -49,8 +49,63 @@ class ModelTrainer:
                 "CatBoosting Regressor":CatBoostRegressor(),
                 "AdaBoost Regressor": AdaBoostRegressor()
             }
+            params={
+                "Decision Tree":{
+                    'criterion':['squared_error','friedman_mse','absolute_error','poisson'],
+                    # 'splitter':['best','random']
+                    # max_features':['sqrt','log2'],
 
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+                },
+                "Random Forest Regressor":{
+                    #'criterion':['squared_error','friedman_mse','absolute_error','poisson'],
+                    # 'max_features':['sqrt','log2',None],
+                   'n_estimators':[8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error','huber','absolute_error','quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error','friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators':[8,16,32,64,128,256],
+                },
+                "Linear Regression": {
+                    'fit_intercept': [True, False],
+                    'copy_X': [True, False],
+                    'n_jobs': [None, 1, 2, 4],
+                    'positive': [False, True],
+                },
+                "Lasso": {
+                    'alpha': [0.01, 0.1, 1.0],
+                    'max_iter': [1000, 2000, 5000],
+                },
+                "Ridge": {
+                    'alpha': [0.01, 0.1, 1.0],
+                    'solver': ['auto', 'svd', 'cholesky', 'lsqr', 'sparse_cg', 'sag', 'saga'],
+                },
+                "K-Neighbors Regressor": {
+                    'n_neighbors': [3, 5, 7, 10],
+                    'weights': ['uniform', 'distance'],
+                    'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute'],
+                    'leaf_size': [10, 20, 30]
+                },
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators':[8,16,32,64,128,256],
+                },
+                "CatBoosting Regressor":{
+                    'depth':[6,8,10],
+                    'learning_rate':[0.01,0.05,0.1],
+                    'iterations':[30,50,100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    #'loss':['linear', 'square','exponential'],
+                    'n_estimators':[8,16,32,64,128,256]
+            }
+            }
+
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,param=params)
 
             best_model_score=max(sorted(model_report.values()))
             best_model_name=list(model_report.keys())[list(model_report.values()).index(best_model_score)]
